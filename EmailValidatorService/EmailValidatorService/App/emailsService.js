a335c2a4f28d4e735fe9fc06b4acca3dfbe64f14
shlomta1 <Shlontz@gmail.com>
2016-05-03 13:51:54 +0300

    Edit Simulator page and addd Angular scripts

---
+++ EmailValidatorService/EmailValidatorService/App/emailsService.js
@@ -0,0 +1,24 @@
+﻿var app = angular.module("MainApp", []);
+
+app.service("emailsService", function ($http, $q) {
+    var deffered = $q.defer();
+
+    $http.get("http://localhost:32145/api/messages/incoming?id="+x).then(function (data) {
+        deffered.resolve(data);
+    });
+    this.getEmails = function ( x) {
+        return deffered.promise;
+    }
+})
+
+.controller("emailsCtrl", function ($scope, userService) {
+    var promise = userService.getEmails();
+
+    promise.then(function (data) {
+        $scope.Emails = data.data;
+        console.log($scope.Emails);
+    });
+})
+
+
+
---
+++ EmailValidatorService/EmailValidatorService/App/userService.js
@@ -0,0 +1,27 @@
+﻿var app = angular.module("MainApp", []);
+
+app.service("userService", function ($http, $q) {
+    var deffered = $q.defer();
+
+    $http.get("http://localhost:32145//api/users?testable=true").then(function (data) {
+        deffered.resolve(data);
+    });
+    this.getUser = function () {
+        return deffered.promise;
+    }
+})
+
+.controller("usersCtrl", function ($scope, userService) {
+    var promise = userService.getUser();
+
+    promise.then(function (data) {
+        $scope.users = data.data;
+        console.log($scope.users);
+    });
+})
+
+
+function SelectUser($scope) {
+    var userId = document.getElementById("userSelector").value;
+    console.log(userId);
+}
